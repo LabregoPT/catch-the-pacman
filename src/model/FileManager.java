bc2c05b7bb6a3778dd484ead478e1ad7628994d8
Jhon Edward Mora <jhonedmora@gmail.com>
2019-03-14 12:09:30 -0500

    Completed some methods in FileManager

--- src/model/FileManager.java
+++ src/model/FileManager.java
@@ -14,8 +14,14 @@ public class FileManager {
 	/**File that holds the data of a saved game*/
 	private File savedGame;
 	
+	/**Path to the saved game file*/
+	private String savedGamePath;
+	
 	/**File that holds the data of all scores scored in the game.*/
 	private File savedScores;
+	
+	/**File to the saved scores file*/
+	private String savedScoresPath;
 	
 	//Relations
 	/**List of all registered scores. Cannot be changed.*/
@@ -24,11 +30,26 @@ public class FileManager {
 	/**
 	 * Creates an instance of FileManager.
 	 * @param gui Relation with class Controller
-	 * @param savedGamePath Path to the file storing the saved game.
-	 * @param savedScoresPath Path to the files storing the saved scores.
+	 * @param sgp Path to the file storing the saved game.
+	 * @param ssp Path to the files storing the saved scores.
+	 * @throws IOException when there's an error loading the files.
+	 * @throws ClassNotFoundException when there's an error casting the class in the files.
 	 */
-	public FileManager(Controller gui, String savedGamePath, String savedScoresPath) {
-		//To do
+	@SuppressWarnings("unchecked")
+	public FileManager(Controller gui, String sgp, String ssp) throws IOException, ClassNotFoundException{
+		savedGamePath = sgp;
+		savedScoresPath = ssp;
+		savedGame = new File(savedGamePath);
+		savedScores = new File(savedScoresPath);
+		
+		//Verifies if there is already a file with the saved scores.
+		if(!savedScores.exists()) {
+			scores = new ArrayList<Score>();
+		}else {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(savedScores));
+			scores = (ArrayList<Score>)ois.readObject();
+			ois.close();
+		}
 	}
 	
 	/**
@@ -50,7 +71,17 @@ public class FileManager {
 	 * @return A String holding scores.
 	 */
 	public String getScores() {
-		return null; //To do
+		String message = "";
+		if(scores.isEmpty()) {
+			message = "There are no currently registered scores.";
+		}else {
+			int j = 1;
+			for (Score i : scores) {
+				message += (j) + i.getScore() + "\n";
+				j++;
+			}
+		}
+		return message;
 	}
 	
 	/**
@@ -62,7 +93,9 @@ public class FileManager {
 	}
 	
 	/**Modifies the saved scores file.*/
-	public void saveScores() {
-		//To do
+	public void saveScores() throws IOException{
+		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream (savedScores));
+		oos.writeObject(scores);
+		oos.close();
 	}
 }
