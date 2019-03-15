97fca264392a955886c7348758b94deb651fef4b
Jhon Edward Mora <jhonedmora@gmail.com>
2019-03-15 10:30:24 -0500

    Added data folder, converted Controller to an appropiate controller class.

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,10 +1,27 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.*;
 import model.*;
 
 public class Controller{
-
+	/** Board where the current game flows.*/
+	@FXML
+	private Pane boardPane;
+	
+	/**Label meant to be used with information, such as current score or new record.*/
+	@FXML
+	private Label informationLabel;
+	
+	/**
+	 * Starts the program and its initial variables.
+	 */
+	@FXML
+	void initialize() {
+		
+	}
 	
 	
 	/**
