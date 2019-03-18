ee4b492a41ae95bf020a76da8a58effa6b5ab79a
Jhon Edward Mora <jhonedmora@gmail.com>
2019-03-18 07:07:02 -0500

    started develop of version 1.1

---
+++ src/model/Directions.java
@@ -0,0 +1,9 @@
+package model;
+
+/** Enumeration holding each of the directions used.
+ * @author Jhon Edward Mora - Universidad ICESI - A00355710
+ * @version 1.1, March/2019
+ */
+public enum Directions {
+	FORWARD,BACKWARD,UPWARD,DOWNWARD;
+}
--- src/model/PacMan.java
+++ src/model/PacMan.java
@@ -1,5 +1,93 @@
 package model;
 
 public class PacMan {
+	
+	//Attributes
+	/**Unique id for this PacMan*/
+	private int id;
+	
+	/**Movement direction.*/
+	private Directions direction;
+	
+	/**Size of the PacMan*/
+	private double size;
+	
+	/**Radius of the PacMan*/
+	private double radius;
+	
+	/**Horizontal position of the PacMan*/
+	private double positionX;
+	
+	/**Vertical position of the PacMan*/
+	private double positionY;
+	
+	/**Number of bounces this PacMan has done*/
+	private int bounces;
+	
+	/**True if this PacMan has been caught, false if not.*/
+	private boolean caught;
+	//Methods
+	
+	/**
+	 * Initializes a new instance of this Class.
+	 * @param dir Direction of movement of this PacMan
+	 * @param x Horizontal position of this PacMan
+	 * @param y Vertical position of this PacMan
+	 * @param s Diameter of the PacMan's body
+	 * @param id Unique identifier for this PacMan
+	 */
+	public PacMan(Directions dir, double x, double y, double s, int id) {
+		this.id = id;
+		direction = dir;
+		size = s;
+		radius = s/2;
+		positionX = x;
+		positionY = y;
+		bounces = 0;
+		caught = false;
+	}
+	
+	/**
+	 * Updates the horizontal and vertical position of this PacMan.
+	 * @param newX The new horizontal position.
+	 * @param newY The new vertical position.
+	 * @param maxX The maximum horizontal position, as set by the board's width.
+	 * @param maxY The maximum vertical position, as set by the board's height.
+	 */
+	public void move(double newX, double newY, double maxX, double maxY) {
+		switch(direction) {
+			case DOWNWARD:
+				
+				break;
+			case FORWARD:
+				
+				break;
+			case UPWARD:
+			
+				break;
+			case BACKWARD:
+				
+				break;
+			default:
+			
+			break;
+			}
+		}
 
+	public double getX() {
+		return positionX;
+	}
+	
+	public double getY() {
+		return positionY;
+	}
+	
+	public void catchIt() {
+		caught = true;
+	}
+	
+	public boolean isCaught() {
+		return caught;
+	}
+	
 }
