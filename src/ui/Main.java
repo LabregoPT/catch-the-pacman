484ba6f9e380703cd1b1dc5658cd896daba6a4c6
Jhon Edward Mora <jhonedmora@gmail.com>
2019-03-21 12:27:03 -0500

    Corrected the Class diagram to match the actual program.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -6,13 +6,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Application app that will launch the GUI.
+ * @author Jhon Edward Mora - Universidad ICESI - A00355710
+ * @version 1.2 - March/2019
+ */
 public class Main extends Application{
 
+	/**
+	 * Main method, starts the program.
+	 * @param args Arguments for the program to be launched.
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
 
-	@Override
+	/**
+	 * Launches the GUI of the program.
+	 */
 	public void start(Stage stage) throws Exception {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("gui.fxml"));
 		Parent root = loader.load();
